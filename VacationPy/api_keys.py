9c593f6c42b3c8d4ef908202b2d15525bf8ac231
angelpopson <angel09popa@gmail.com>
2020-08-26 17:24:53 -0400

    Final Version

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,3 @@
+
+# Google API Key
+g_key = "AIzaSyDPUBQpNdUUg8mGpALGm95LK4x2ZNYyz1o"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "a91ff6e71b827e031d6c201602dbe0d4"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
